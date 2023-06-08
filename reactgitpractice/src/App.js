12fde6aa728525dbec20caa9a0785de395713354
garimachandna <gchandna4@gmail.com>
2023-06-08 23:30:28 +0530

    demo app structure

--- reactgitpractice/src/App.js
+++ reactgitpractice/src/App.js
@@ -1,11 +1,34 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/homepage";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Policy from "./pages/policy";
+import Pagenotfound from "./pages/pagenotfound";
+import Register from "./pages/auth/register";
+import Login from "./pages/auth/login";
+
+import Dashboard from "./pages/user/dashboard";
+import PrivateRoute from "./components/routes/private";
+import ForgotPassword from "./pages/auth/forgotpassword";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi, I edited the react code</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="" element={<Dashboard />} />
+        </Route>
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />}></Route>
+        <Route path="*" element={<Pagenotfound />}></Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ reactgitpractice/src/components/layout.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import Footer from "./footer";
+import Header from "./header";
+
+const Layout = ({ children, title, description, keywords }) => {
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+
+        <title>{title}</title>
+      </Helmet>
+
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+Layout.defaultProps = {
+  title: "React Git Practice",
+  description: "React Git Practice",
+  keywords: "React Git Practice",
+};
+
+export default Layout;
