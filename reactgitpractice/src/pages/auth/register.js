998409281595c8b38128b77991383cb94bf1fe85
garimachandna <gchandna4@gmail.com>
2023-06-09 16:40:34 +0530

    modified homepage and register page

--- reactgitpractice/src/pages/auth/register.js
+++ reactgitpractice/src/pages/auth/register.js
@@ -1,7 +1,37 @@
 import React from "react";
+import Layout from "../../components/layout";
+import "../../styles/AuthStyles.css";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <Layout>
+      <div className="container">
+        <h1>Register</h1>
+        <p>Enter your email and password to register</p>
+        <form action method="post">
+          <div className="box">
+            <i className="fa-solid fa-envelope" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter Your Email"
+            />
+          </div>
+          <div className="box">
+            <i className="fa-solid fa-key" />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter Your Password"
+            />
+          </div>
+          <button className="btn">Sign Up</button>
+        </form>
+      </div>
+    </Layout>
+  );
 };
 
 export default Register;
